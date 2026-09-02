97942533504f48122165a76618854900f9de72d0
Lazare-Panam <sales@panamvalves.co.uk>
2026-09-02 10:40:35 +0100

    feat:added-add-to-cart-validator

---
+++ Mars.API/Models/ModelValidators/AddToCartRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Mars.API.Models.Basket;
+namespace Mars.API.Models.ModelValidators
+{
+    public class AddToCartRequestValidator : AbstractValidator<AddToCartRequest>
+    {
+        public AddToCartRequestValidator() 
+        {
+            RuleFor(x => x.Quantity).NotEmpty().GreaterThan(0).WithMessage("Please specify the quantity to be greater than 0");
+        }
+    }
+}

400c6140e81f5d4feee96198049034ca1f3ad1d1
Lazare-Panam <sales@panamvalves.co.uk>
2026-09-02 10:52:11 +0100

    feat:updated-validation-class

--- Mars.API/Models/ModelValidators/AddToCartRequestValidator.cs
+++ Mars.API/Models/ModelValidators/AddToCartRequestValidator.cs
@@ -6,7 +6,11 @@ namespace Mars.API.Models.ModelValidators
     {
         public AddToCartRequestValidator() 
         {
-            RuleFor(x => x.Quantity).NotEmpty().GreaterThan(0).WithMessage("Please specify the quantity to be greater than 0");
+            RuleFor(x => x.SeriesId).NotEmpty().WithMessage("SeriesId is required.");
+            RuleFor(x => x.VariantId).NotEmpty().WithMessage("VariantId is required.");
+            RuleFor(x => x.ProductDescription).NotEmpty().MaximumLength(500).WithMessage("ProductDescription is required and must not exceed 500 characters.");
+            RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be a positive number.");
+            RuleFor(x => x.PictureUrl).NotEmpty().Must(url => Uri.TryCreate(url, UriKind.Absolute, out _)).WithMessage("PictureUrl must be a valid absolute URL.");
         }
     }
 }
